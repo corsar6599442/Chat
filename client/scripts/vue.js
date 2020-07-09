86f3686d2425d039a499113e283bf07aa351f139
Mikhail_Zakharov <corsar6599442@gmail.com>
2020-07-09 23:45:25 +0300

    Created a vue instance

--- client/scripts/vue.js
+++ client/scripts/vue.js
@@ -4,5 +4,17 @@ let chat = new Vue ({
         state: 'default',
         header: 'Наш чат',
         newMessage: ''
-    }
+    },
+    methods: {
+        saveItem: function() {
+            this.items.push({
+                label: this.newMessage,
+                purchased: false,
+            });
+            this.newMessage = '';
+    },
+    changeState: function(newState) {
+        this.state = newState;
+        this.newItem = '';
+    },
 })
